4f98896c815c2b7ffa4a03d20dc7703c2f504891
Imran Pollob <polboy777@gmail.com>
2017-04-08 13:02:23 +0600

    add string solutions

--- Strings/find_a_string.py
+++ Strings/find_a_string.py
@@ -1,9 +1,8 @@
-import re
-
 def count_substring(string, sub_string):
     count = 0
-    for i in re.finditer(sub_string, string):
-        count +=1
+    for i in range(len(string)-len(sub_string)+1):
+        if (string[i:i+len(sub_string)] == sub_string):
+            count += 1
     return count
 
 if __name__ == '__main__':
